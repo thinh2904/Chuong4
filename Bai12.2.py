0cf5b5f5e77c2f87e0dec843cf207a8c68e0d482
thinh2904 <72325160+thinh2904@users.noreply.github.com>
2020-11-28 19:15:14 +0700

    Add files via upload

---
+++ Bai12.2.py
@@ -0,0 +1,15 @@
+import random
+import string
+#Bảng chữ in thường
+a=string.ascii_lowercase
+#Bảng chữ in hoa
+b= string.ascii_uppercase
+#Chọn ngẫu nhiên 1 chữ in hoa
+t=random.choice(b)
+#Random tuổi
+age=random.randrange(1,100)
+#Tạo độ dài của tên từ 2 đến 5 chữ
+for i in range(random.randrange(1,5)):
+	t=t+random.choice(a)
+dict={'name':t,'age':age}
+print(dict)
